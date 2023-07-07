13cc8064b9f2debb04256dee650e833c116d3a4d
GonzalezLE <gonzalez-3216@hotmail.com>
2023-07-06 22:55:18 -0600

    my hello word :D

---
+++ src/main/java/com/codeapps/CodeappsApplication.java
@@ -0,0 +1,13 @@
+package com.codeapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeappsApplication.class, args);
+	}
+
+}
