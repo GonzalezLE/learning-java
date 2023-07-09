7ae9a343991dc6ad72a28e22e48f71eb6b985e1f
GonzalezLE <gonzalez-3216@hotmail.com>
2023-07-09 17:37:59 -0600

    creando entidan con llave primaria compuesta

--- src/main/java/com/codeapps/persistence/entity/Producto.java
+++ src/main/java/com/codeapps/persistence/entity/Producto.java
@@ -1,10 +1,6 @@
 package com.codeapps.persistence.entity;
-import org.springframework.data.annotation.Id;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "productos")
