f543e1748d7c59012843af3b5b0c4828330b455e
GonzalezLE <gonzalez-3216@hotmail.com>
2023-07-09 00:09:18 -0600

    mapeando tablas simples

---
+++ src/main/java/com/codeapps/persistence/entity/Categoria.java
@@ -0,0 +1,43 @@
+package com.codeapps.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer id_categoria;
+
+    private String descripcion;
+    private Boolean estado;
+    
+    public Integer getId_categoria() {
+        return id_categoria;
+    }
+    public void setId_categoria(Integer id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public Boolean getEstado() {
+        return estado;
+    }
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    
+
+}
